d8ccb2d49c0a99c401565cec5d75b49f545c262e
taku0875 <always092471@gmail.com>
2025-06-11 00:14:21 +0900

    Update page.jsx

--- src/app/customers/create/page.jsx
+++ src/app/customers/create/page.jsx
@@ -1,10 +1,39 @@
-import { Suspense } from "react";
-import ConfirmPageClient from "./ConfirmPageClient";
+use client";
+import { useRef } from "react";
+import { useRouter } from "next/navigation";
 
-export default function Page() {
+import createCustomer from "./createCustomer";
+
+export default function CreatePage() {
+  const formRef = useRef();
+  const router = useRouter();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const formData = new FormData(formRef.current);
+    const customerId = formData.get("customer_id")?.trim();
+
+    if (!customerId) {
+      alert("Customer IDは必須です。入力してください。");
+      return;
+    }
+
+    await createCustomer(formData);
+    router.push(`./create/confirm?customer_id=${customerId}`);
+  };
+
   return (
-    <Suspense fallback={<div>読み込み中...</div>}>
-      <ConfirmPageClient />
-    </Suspense>
-  );
-}
+    <>
+      <div className="card bordered bg-white border-blue-200 border-2 max-w-md m-4">
+        <div className="m-4 card bordered bg-blue-200 duration-200 hover:border-r-red">
+          <form ref={formRef} onSubmit={handleSubmit}>
+            <div className="card-body">
+              <h2 className="card-title">
+                <p>
+                  <input
+                    type="text"
+                    name="customer_name"
+                    placeholder="桃太郎"
+                    className="input input-bordered"
+                  />
+                </p>
